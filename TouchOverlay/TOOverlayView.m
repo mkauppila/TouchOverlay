b38888be3672ff7b9cc8b908181e2f2d181610ae
Markus Kauppila <markus.kauppila@gmail.com>
2013-07-22 18:47:45 +0300

    Add TOOverlayView
    The touches will be rendered to this view.

---
+++ TouchOverlay/TOOverlayView.m
@@ -0,0 +1,31 @@
+//
+//  TOOverlayView.m
+//  TouchOverlayExample
+//
+//  Created by markus on 22.7.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import "TOOverlayView.h"
+
+@implementation TOOverlayView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
