79fa518faa351c295f19ed26a016762c65971e4b
Markus Kauppila <markus.kauppila@gmail.com>
2013-07-22 18:44:46 +0300

    Add TOApplication

---
+++ TouchOverlay/TOApplication.m
@@ -0,0 +1,18 @@
+//
+//  TOApplication.m
+//  TouchOverlayExample
+//
+//  Created by markus on 22.7.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import "TOApplication.h"
+
+@implementation TOApplication
+
+- (void)sendEvent:(UIEvent *)event
+{
+	[super sendEvent:event];
+}
+
+@end
